2702d82930970b7796d0e4dfc915e60f880937eb
Arthur.Oliveira <arthur.oliveira@grupoicts.com.br>
2022-05-03 23:29:50 -0300

    post screen by slug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home({ product }:HomeProps) {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Home | Blog</title>
       </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,30 +1,71 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client'
+import Link from 'next/link';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return <>
         <Head>
-            <title>Posts</title>
+            <title>Posts | Blog</title>
         </Head>
 
         <main className={styles.container}>
             <div className={styles.posts}>
-                <a href='#'>
-                    <time>01 de maio de 2022</time>
-                    <strong>titulo do post</strong>
-                    <p>conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post </p>
-                </a>
-                <a href='#'>
-                    <time>01 de maio de 2022</time>
-                    <strong>titulo do post</strong>
-                    <p>conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post </p>
-                </a>
-                <a href='#'>
-                    <time>01 de maio de 2022</time>
-                    <strong>titulo do post</strong>
-                    <p>conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post conteudo do post </p>
-                </a>
+                {posts.map(post => (
+                    <Link href={`/posts/${post.slug}`}>
+                        <a key={post.slug} href='#'>
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    </Link>
+                ))}
             </div>
         </main>
     </>
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+
+    const response: any = await prismic.query([
+        Prismic.predicates.at('document.type', 'post')
+    ], {
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100
+    })
+
+    const posts = response.results.map((post: any) => {
+        return {
+            slug: post.uid,
+            title: post.data.Title,
+            excerpt: post.data.Content.find((content: any) => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        };
+    })
+
+    console.log('teste', posts)
+
+    return {
+        props: {
+            posts
+        }
+    }
 }
